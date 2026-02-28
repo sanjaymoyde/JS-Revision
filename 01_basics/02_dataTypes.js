e7eb7af52ce8138b28c516f5e50f6da7a2740297
Sanjay Moyde <sanjaymoydey00786@gmail.com>
2026-02-28 00:48:53 +0000

    basic data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strick"; //treall all the js code as newer version
+
+let name = "Sanjay"
+let age = 23
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+// string => ""
+// boolean => true/false
+//null => it is a standalone value, represent empty value = no value
+// undefined => the variable is not assigned with any value or initialized.
+//symbol => unique
+
+
+
+console.log(typeof name);
+
+console.log(typeof(null)); //object
+
+console.log(typeof undefined); //undefined
+
+
