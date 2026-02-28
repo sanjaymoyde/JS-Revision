b6a9833476fdcd815d13f52e5bfc5f3cd78a74ac
Sanjay Moyde <sanjaymoydey00786@gmail.com>
2026-02-28 01:28:21 +0000

    conversion_operations added

---
+++ 01_basics/03_conversion_operations.js
@@ -0,0 +1,38 @@
+//Data Type Conversions
+
+let score =  false//true//"sanjay"//null //"22abc"
+
+//console.log(typeof score) // 33 -> number
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber); //number
+//console.log(valueInNumber)
+
+
+// "33" => 33
+//"33abc" => NaN
+//true => 1 ; false => 0
+
+//Boolean conversion
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+// 1 => true
+// 0 => false
+// "" => false
+//"sanjay" => true
+
+let somenumber = 23
+
+let stringNumber = String(somenumber)
+
+console.log(typeof stringNumber)
+
+console.log(stringNumber);
+
+
