d7b677f5110d62d70a783bb7f56d7a1efeabd411
Sanjay Moyde <sanjaymoydey00786@gmail.com>
2026-03-11 02:45:08 +0000

    add array manipulation examples

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,49 @@
+// const arr = [1, 2, 3, 4, 5];
+
+// //console.log(arr); //[ 1, 2, 3, 4, 5 ]
+
+// //console.log(typeof arr); //object
+
+// //console.log(Array.isArray(arr)); //true
+
+// //console.log(arr[0]); //1
+// //console.log(arr[4]); //5
+// //console.log(arr[5]); //undefined
+
+// arr[5] = 6; // we can add new element in array by assigning value to new index
+
+// //console.log(arr); //[ 1, 2, 3, 4, 5, 6 ]
+
+// arr.push(7); // we can also add new element in array by using push method
+
+// //console.log(arr); //[ 1, 2, 3, 4, 5, 6, 7 ]
+
+// arr.unshift(0); // we can also add new element in array at the beginning by using unshift method
+
+// //console.log(arr); //[ 0, 1, 2, 3, 4, 5, 6, 7 ]
+
+// arr[0] = -1; // we can also change the value of existing element in array by assigning new value to existing index
+
+// //console.log(arr); //[ -1, 1, 2, 3, 4, 5, 6, 7 ]
+
+// arr.pop(); // we can remove the last element from array by using pop method
+
+// //console.log(arr); //[ -1, 1, 2, 3, 4, 5, 6 ]
+
+// arr.shift(); // we can remove the first element from array by using shift method
+
+// //console.log(arr); //[ 1, 2, 3, 4, 5, 6 ]
+
+// arr.splice(2, 1); // we can remove element from array by using splice method (index, number of elements to remove)
+
+// //console.log(arr); //[ 1, 2, 4, 5, 6 ]
+
+// arr.splice(2, 0, 3); // we can also add element in array by using splice method (index, number of elements to remove, element to add)
+
+// //console.log(arr); //[ 1, 2, 3, 4, 5, 6 ]
+
+// arr.splice(2, 1, 10); // we can also change the value of existing element in array by using splice method (index, number of elements to remove, element to add)
+
+// //console.log(arr); //[ 1, 2, 10, 4, 5, 6   ]   
+
+
